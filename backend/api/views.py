5856c2c60aae9919dc43bf0e9569076e9768a2b9
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2021-06-17 02:23:28 -0400

    Add swagger

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,6 +7,10 @@ from .serializers import FileSerializer
 
 class UploadFileView(APIView):
     def post(self, request, *args, **kwargs):
+        """
+        Send a .txt file, using "file" as key. The server will respond
+        with the number of occurrences for each word within the .txt
+        """
         file_serializer = FileSerializer(data=request.data)
         if file_serializer.is_valid():
             instance = file_serializer.save()
--- backend/wordcount_project/urls.py
+++ backend/wordcount_project/urls.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Wordcount API',
+        default_version='v1',
+        description='An API for counting words within a .txt file',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='alvaradosegurajorge@gmail.com'),
+        license=openapi.License(name='BSD License'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0
+    ), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0
+    ), name='schema-redoc'),
 ]
